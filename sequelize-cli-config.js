74feeccb403080e9270187d3dfc71cdde7b001d3
Seiichi Okumiya <occmee@gmail.com>
2017-02-03 16:53:00 +0900

    Modified sequelize-cli config

--- sequelize-cli-config.js
+++ sequelize-cli-config.js
@@ -2,24 +2,27 @@ require("dotenv").config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.RRSST_DB_USER,
+    "password": process.env.RRSST_DB_PASS,
+    "database": process.env.RRSST_DB_NAME,
+    "host": process.env.RRSST_DB_HOST,
+    "port": process.env.RRSST_DB_PORT,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME_TEST,
-    "host": process.env.DB_HOST,
+    "username": process.env.RRSST_DB_USER,
+    "password": process.env.RRSST_DB_PASS,
+    "database": process.env.RRSST_DB_NAME_TEST,
+    "host": process.env.RRSST_DB_HOST,
+    "port": process.env.RRSST_DB_PORT,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.RRSST_DB_USER,
+    "password": process.env.RRSST_DB_PASS,
+    "database": process.env.RRSST_DB_NAME,
+    "host": process.env.RRSST_DB_HOST,
+    "port": process.env.RRSST_DB_PORT,
     "dialect": "mysql"
   }
 };
